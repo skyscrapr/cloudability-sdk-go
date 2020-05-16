049e29cd4cdcb0ef65e4cc7ec54191c73ce0679b
Richard Weerasinghe <rich.w@me.com>
2020-05-16 18:26:22 +1200

    Initial Implmentation

---
+++ cloudability/rightsizing.go
@@ -0,0 +1,37 @@
+package cloudability
+
+import (
+	"fmt"
+)
+
+const rightsizingEndpoint = "/v3/rightsizing/"
+
+// RightsizingEndpoint Cloudability Rightsizing Endpoint
+type RightsizingEndpoint struct {
+	*v3Endpoint
+}
+
+// Rightsizing Endpoint
+func (c *Client) Rightsizing() *RightsizingEndpoint {
+	return &RightsizingEndpoint{newV3Endpoint(c, rightsizingEndpoint)}
+}
+
+// Resource Cloudability Rightsizing Resource
+type Resource struct {
+	Vendor string `json:"vendor"`
+	Service string `json:"service"`
+	ResourceIdentifier string `json:"resourceIdentifier"`
+	Recommendations []Recommendation `json:"recommendations"`
+}
+
+// Recommendation Cloudability Rightsizing Resource Recommendation
+type Recommendation struct {
+	Action string `json:"action"`
+}
+
+// GetResource return a Rightsizing Resource
+func (e RightsizingEndpoint) GetResource(vendor string, service string, resourceID string) (*Resource, error) {
+	var resource Resource
+	err := e.get(e, fmt.Sprintf("%s/recommendations/%s?filters=resourceIdentifier==%s", vendor, service, resourceID), &resource)
+	return &resource, err
+}
